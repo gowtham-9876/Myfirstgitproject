48b53d65e238d07ecd6856012e6545958a3a009e
Gowtham <Gowtham@gmail.com>
2025-06-16 17:49:50 +0530

    updated index.py with new logic

--- myfirstgitproject.py
+++ myfirstgitproject.py
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
-echo 'print("Hi im Gangothri!")' > myfirstproject.py
+echo 'print("Hlo im Gangothri!")' > myfirstproject.py
 
 =======
-echo 'print("Hi im Gangothri!")' > myfirstproject.py
+echo 'print("Hii im Gangothri!")' > myfirstproject.py
 
 >>>>>>> 583b834e0263a90ee308d875055195330aee4e87
